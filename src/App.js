dc2bb93ec3e6bf2da94afb5e39af5f93de0b3774
ichido <ichido@live.jp>
2020-06-02 18:02:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [test, setTest] = useState(0);
+
+  // const countUpMethod = () => {
+  //   setTest( test + 1 )
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {test}
+      <button onClick={ () => setTest(test + 1) }>Click</button>
+      {/* <button onClick={countUpMethod}>Click</button> */}
     </div>
   );
 }
-
-export default App;
